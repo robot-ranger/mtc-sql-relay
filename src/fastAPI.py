13de42806b8ecc6ad3f95dda3aad59ad5856f389
robot-ranger <ted@processrobotics.com>
2023-10-13 16:32:03 -0700

    query

--- src/fastAPI.py
+++ src/fastAPI.py
@@ -13,9 +13,7 @@ import uvicorn
 description = """
 ![img](/static/process_robotics.png)
 
-## Publish & Subscribe to MQTT topics
-
-Subscribe to MQTT topics and store the messages to a database.
+## Subscribe to MQTT topics and store the messages in a remote DB.
 
 """
 
@@ -26,7 +24,7 @@ tags_metadata = [
 	},
 	{
 		"name": "database",
-		"description": "Configure the database settings."
+		"description": "Configure database settings."
 	}
 ]
 
@@ -43,11 +41,11 @@ app = FastAPI(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Configure the logger
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 # Default broker address and port
-DEFAULT_BROKER_ADDRESS = "localhost"
+DEFAULT_BROKER_ADDRESS = "192.168.0.105"
 DEFAULT_BROKER_PORT = 1883
 
 # Default SQLite database file
@@ -74,6 +72,7 @@ class MQTTConnector:
 		self.db_password = db_password
 		self.db_name = db_name
 		self.mqtt_client = mqtt.Client()
+		# self.mqtt_client = FastMQTT(config=MQTTConfig(host=self.broker_address, port=self.broker_port))
 		self.conn = self.create_database_connection()
 
 	def create_database_connection(self):
@@ -126,7 +125,7 @@ class MQTTConnector:
 		cursor.close()
 		conn.close()
 
-	def on_message(self, client, userdata, msg):
+	def on_message(self, client, userdata, msg, store=True):
 		# Parse the published message
 		payload = msg.payload.decode()
 		json_data = json.loads(payload)
@@ -189,11 +188,38 @@ class MQTTConnector:
 				logger.warning(f"Failed to unsubscribe from topic '{topic}'")
 		return {"message": "Unsubscribe Successful!"}
 
-	def query_mqtt(self, topics: List[str], qos: int = 0):
-		for topic in topics:
-			self.mqtt_client.publish(topic, payload="", qos=qos, retain=False)
-			logger.info(f"Sent query for topic '{topic}'")
-		return {"message": "Query sent successfully!"}
+	def query_topic(self, topic: str):
+		logger.info(f"Querying topic '{topic}'")
+		try:
+			logger.info(f"Connecting to MySQL database '{self.db_name}'")
+			# query mysql database
+			conn_mysql = mysql.connector.connect(
+				host=self.db_host,
+				port=self.db_port,
+				user=self.db_user,
+				password=self.db_password,
+				database=self.db_name
+			)
+			cursor_mysql = conn_mysql.cursor()
+			logger.info(f"Executing query on MySQL database '{self.db_name}'")
+			# return the latest data item for the topic
+			cursor_mysql.execute(
+				f"""SELECT value
+				FROM {DEFAULT_TABLE_NAME}
+				WHERE topic = '{topic}'
+				ORDER BY id DESC
+				LIMIT 1;"""
+			)
+			result = cursor_mysql.fetchall()
+			logger.debug(f"Query result: {result}")
+			cursor_mysql.close()
+			conn_mysql.close()
+			return {"message": "Query sent successfully!",
+					"result": result}
+		except Exception as e:
+			logger.warning(f"Failed to query topic '{topic}'")
+			logger.debug(f"Exception: {e}")
+			return {"message": f"Failed to query topic '{topic}'"}
 
 	def get_subscribed_topics(self):
 		cursor = self.conn.cursor()
@@ -304,20 +330,23 @@ async def unsubscribe_topics(topics: List[str] = Query(...)):
 	return result
 
 
-@app.post("/query", tags=["mqtt client"])
-async def query_topics(topics: List[str] = Query(...), qos: Optional[int] = Query(0)):
+@app.get("/query", tags=["mqtt client"])
+async def query_topics(topic: str = Query(...), qos: Optional[int] = Query(0)):
 	"""Send a query for MQTT topics. Must include full topic path, e.g. 'MTConnect/Observation/{device_id}/Controller/Events/Program'.
 
 	Args:
-		topics (List[str]): List of topics to query
+		topics (str): List of topics to query
 
 	Returns:
 		dict: Message confirming that the query was sent successfully"""
-	result = mqtt_connector.query_mqtt(topics, qos)
-	return result
+	try:
+		return mqtt_connector.query_topic(topic)
+	except TimeoutError as te:
+		logger.error(f"TimeoutError: {te}")
+		return {"message": "Timed out. No retained messages found."}
 
 
-@app.post("/publish", tags=["mqtt client"])
+@app.post("/publish", tags=["mqtt client"], include_in_schema=False)
 async def publish_message(topic: str = Query(...), message: str = Query(...)):
 	"""Publish a message on an MQTT topic. Must include full topic path, e.g. 'MTConnect/Observation/{device_id}/Controller/Events/Program'.
 
@@ -344,6 +373,6 @@ async def get_subscribed_topics():
 
 if __name__ == "__main__":
 	try:
-		uvicorn.run("__main__:app", host="0.0.0.0", port=8000, reload=True)
+		uvicorn.run("__main__:app", host="0.0.0.0", port=8000, reload=True, log_level="debug")
 	except KeyboardInterrupt:
 		logger.info("Keyboard interrupt received. Stopping the application...")
